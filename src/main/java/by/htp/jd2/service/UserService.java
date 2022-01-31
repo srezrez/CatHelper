70b18aa8bd836aa39059958232efb9696994c1a2
Svetlana Reznichenko <srezrez@gmail.com>
2022-01-31 22:38:41 +0300

    fixed UserService

--- src/main/java/by/htp/jd2/service/UserService.java
+++ src/main/java/by/htp/jd2/service/UserService.java
@@ -1,9 +1,9 @@
 package by.htp.jd2.service;
 
-import by.htp.jd2.entity.UserInfo;
+import by.htp.jd2.entity.User;
 
 public interface UserService {
 
     String signIn(String login, String password) throws ServiceException;
-    boolean signUp(UserInfo userInfo);
+    boolean signUp(User userInfo);
 }

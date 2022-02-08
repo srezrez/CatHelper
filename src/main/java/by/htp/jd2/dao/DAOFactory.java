950faccd47af4b9dd20a53cda22770b6fabd7f3a
Svetlana Reznichenko <srezrez@gmail.com>
2022-02-08 21:08:47 +0300

    completed dao factory

--- src/main/java/by/htp/jd2/dao/DAOFactory.java
+++ src/main/java/by/htp/jd2/dao/DAOFactory.java
@@ -1,16 +1,36 @@
 package by.htp.jd2.dao;
 
-import by.htp.jd2.dao.impl.UserDAOImpl;
+import by.htp.jd2.dao.impl.*;
 
 public class DAOFactory {
 
     private static final DAOFactory instance = new DAOFactory();
     private final UserDAO userDAO = new UserDAOImpl();
+    private final CatDAO catDAO = new CatDAOImpl();
+    private final RequestDAO requestDAODAO = new RequestDAOImpl();
+    private final DocumentDAO documentDAO = new DocumentDAOImpl();
+    private final BreedDAO breedDAO = new BreedDAOImpl();
 
     private DAOFactory() {}
 
     public UserDAO getUserDAO() {
         return userDAO;
+    }
+
+    public CatDAO getCatDAO() {
+        return catDAO;
+    }
+
+    public RequestDAO getRequestDAODAO() {
+        return requestDAODAO;
+    }
+
+    public DocumentDAO getDocumentDAO() {
+        return documentDAO;
+    }
+
+    public BreedDAO getBreedDAO() {
+        return breedDAO;
     }
 
     public static DAOFactory getInstance() {
